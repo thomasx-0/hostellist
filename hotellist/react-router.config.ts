c98b242d39cdf4d7a1fc4f342a66915cf9b09d8a
Thomas Pennant <127155153+thomasx-0@users.noreply.github.com>
2025-07-29 01:14:59 +0000

    fix: update react-router config to ensure type generation for routes

--- hotellist/react-router.config.ts
+++ hotellist/react-router.config.ts
@@ -4,4 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  // Enable type generation for routes
+  typescript: true,
 } satisfies Config;
